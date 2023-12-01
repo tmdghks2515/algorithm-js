a8ca9eeebf88cfb784168eaabed65608e0ad6fe6
imseunghwan <tmdghks2515@idrsystems.co.kr>
2023-12-01 13:56:47 +0900

    greedy lv1 모범답 추가

--- greedy/greedy.lv1.js
+++ greedy/greedy.lv1.js
@@ -45,3 +45,30 @@ function solution2(n, lost, reserve) {
     reserve = reserve.filter(r => r !== b)
   }).length
 }
+
+// [모범 답]
+function solution3(n, lost, reserve) {
+  const students = {};
+  let answer = 0;
+  for(let i = 1; i <= n; i++){
+    students[i] = 1;
+  }
+  lost.forEach(number => students[number] -= 1);
+  reserve.forEach(number => students[number] += 1);
+
+  for(let i = 1; i <= n; i++){
+    if(students[i] === 2 && students[i-1] === 0){
+      students[i-1]++;
+      students[i]--;
+    } else if(students[i] === 2 && students[i+1] === 0){
+      students[i+1]++;
+      students[i]--;
+    }
+  }
+  for(let key in students){
+    if(students[key] >= 1){
+      answer++;
+    }
+  }
+  return answer;
+}
